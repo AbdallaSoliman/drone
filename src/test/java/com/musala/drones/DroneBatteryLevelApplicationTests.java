5f5f4152018476d468b444aab955a0a0c0370dc1
Abdalla SHEHATA <Abdalla.SHEHATA@idemia.com>
2022-10-02 01:18:23 +0200

    junit for available drones for loading drone battery level for a given drone

--- src/test/java/com/musala/drones/DroneBatteryLevelApplicationTests.java
+++ src/test/java/com/musala/drones/DroneBatteryLevelApplicationTests.java
@@ -32,10 +32,5 @@ class DroneBatteryLevelApplicationTests {
         .andDo(print())
         .andExpect(status().isOk())
             .andExpect(content().string("38.0"));
-    //        .andExpect(
-    //            content()
-    //                .json(
-    //
-    // "{\"content\":[{\"id\":\"7d00bb7c-ce60-472f-81dd-5d1ff3c0dc4f\",\"serial\":\"1\",\"model\":\"LIGHT_WEIGHT\",\"weightLimit\":50.0,\"batteryCapacity\":77.0,\"state\":\"IDLE\"},{\"id\":\"50f4c99b-0ee3-4c4b-aa73-ee3523a3364a\",\"serial\":\"9\",\"model\":\"HEAVY_WEIGHT\",\"weightLimit\":500.0,\"batteryCapacity\":42.0,\"state\":\"IDLE\"},{\"id\":\"b7f799ad-585a-4404-a94b-e6c78c841b6c\",\"serial\":\"10\",\"model\":\"MIDDLE_WEIGHT\",\"weightLimit\":140.0,\"batteryCapacity\":100.0,\"state\":\"IDLE\"}],\"pageable\":{\"sort\":{\"empty\":true,\"sorted\":false,\"unsorted\":true},\"offset\":0,\"pageSize\":20,\"pageNumber\":0,\"paged\":true,\"unpaged\":false},\"totalPages\":1,\"last\":true,\"totalElements\":3,\"size\":20,\"number\":0,\"sort\":{\"empty\":true,\"sorted\":false,\"unsorted\":true},\"first\":true,\"numberOfElements\":3,\"empty\":false}"));
-  }
+ }
 }
